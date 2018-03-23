509c92e36c961ed5b9ebc2c4e301f6f90e294595
Michael Collins <michaelc@boxdice.com.au>
2018-03-23 14:53:27 +0100

    package.json and initial files

---
+++ animation.js
@@ -0,0 +1,82 @@
+var app = new PIXI.Application(800,800)
+
+// 2. Append canvas element to the body
+document.body.appendChild(app.view);
+
+var shipsize = 15;
+
+function render(){
+  var ship = new Ship(0,app.view.width/2,app.view.height/2,0,0);
+  ship.rotate(6)
+  // ship.move(100,-100);
+  ship.draw()
+}
+
+function drawCircle(x, y, size){
+    var circle = new PIXI.Graphics();
+    circle.lineStyle(1, 0xFFFFFF, 1);
+    // draw circle (x, y, radius)
+    circle.drawCircle(x, y, size);
+    app.stage.addChild(circle);
+}
+
+function drawTriangle(x,y){
+    // 4d. Create a triangle
+    var triangle = new PIXI.Graphics();
+    triangle.lineStyle(1, 0xFFFFFF, 1);
+    triangle.moveTo(x+shipsize,y+shipsize);
+    triangle.lineTo(x, y-shipsize);
+    triangle.lineTo(x-shipsize, y+shipsize);
+    triangle.lineTo(x+shipsize, y+shipsize);
+    
+    app.stage.addChild(triangle);
+}
+
+
+function drawTriangle(matrix){
+  // 4d. Create a triangle
+  var triangle = new PIXI.Graphics();
+  triangle.lineStyle(1, 0xFFFFFF, 1);
+  triangle.moveTo(matrix.a, matrix.b);
+  triangle.lineTo(matrix.c, matrix.d);
+  triangle.lineTo(matrix.tx, matrix.ty);
+  triangle.lineTo(matrix.a, matrix.b);
+  
+  app.stage.addChild(triangle);
+}
+
+class Ship {
+  constructor(angle,xPos,yPos,xVec,yVec){
+    this.angle = angle;
+    this.centerx = xPos;
+    this.centery = yPos;
+    this.matrix =  new PIXI.Matrix(xPos+shipsize,yPos+shipsize,xPos,yPos-shipsize, xPos-shipsize,yPos+shipsize);
+    this.xVec = xVec;
+    this.yVec = yVec;
+  };
+
+  draw(){
+    // drawTriangle(this.center.tx, this.center.ty)
+    drawTriangle(this.matrix);
+  };
+
+  rotate(degrees){
+    this.move(-this.centerx, -this.centery)
+    this.matrix.rotate(degrees)
+    this.move(this.centerx, this.centery)
+  }
+
+  move(x,y){
+    this.matrix.set(this.matrix.a+x,this.matrix.b+y,this.matrix.c+x,this.matrix.d+y,this.matrix.tx+x, this.matrix.ty+y)
+  }
+}
+
+// Ship.prototype = {
+//   centerx : function(){
+//     return this.center.a - shipsize;
+//   },
+
+//   centery: function() {
+//     return this.center.b - shipsize;
+//   }
+// }
